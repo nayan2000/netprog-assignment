959f27709c862770fc9faf9f39b136d135d6af63
abdulkk49 <kadirkhimani5@gmail.com>
2021-03-27 20:23:28 +0530

    Remove reverse hashmap

--- P3/hashmap.h
+++ P3/hashmap.h
@@ -25,24 +25,5 @@ int get(hashmap*, char*);
 int has_key(hashmap*, char*);
 
 
-typedef struct bucket_node_t_r{
-    int key;
-	char val[20];
-	struct bucket_node_t_r *next;
-}bucket_node_r;
-
-typedef struct hashmap_r_t{
-	bucket_node_r **buckets;
-	size_t capacity;
-}hashmap_r;
-
-size_t hash_r(int, size_t);
-void create_hm_r(hashmap_r*, size_t);
-void free_bucket_r(bucket_node_r*);
-void free_hm_r(hashmap_r*);
-void insert_r(hashmap_r*, int, char*);
-bucket_node_r* get_bucket_r(hashmap_r*, int);
-char* get_r(hashmap_r*, int);
-int has_key_r(hashmap_r*, int);
 
 #endif
