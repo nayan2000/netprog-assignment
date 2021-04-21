1e420f6822306eed7c2577c43e54865059777a11
unknown <ayush.prog@gmail.com>
2021-04-22 00:17:00 +0530

    Implementing some menu options

--- Assignment-2/P3/group_comm.c
+++ Assignment-2/P3/group_comm.c
@@ -1,10 +1,11 @@
 #include "basic.h"
-#include "hashmap.h"
+#include "hashmap.c"
 
 #define BUFLEN 512
 #define PORT 33777
 
 #define MAX_GROUP_LIMIT 10
+#define BROADCAST_TIMEOUT 1
 
 void readLine(char* msg) {
 	char in = 'a';
@@ -15,14 +16,13 @@ void readLine(char* msg) {
 	}
 }
 
-char* genMulticastIP() {
-    char* ip = malloc(16); 
+void genMulticastIP(char* ip) {
+    ip = malloc(16); 
     strcpy(ip, "239.0.0.");
     char l[3];
     int a = (rand() % 254) + 1;
     sprintf(l, "%d", a);
     strcat(ip, l);
-    return ip;
 }
 
 void sendGrpMsg(char* dest_ip, char msg[BUFLEN]) {
@@ -35,7 +35,7 @@ void sendGrpMsg(char* dest_ip, char msg[BUFLEN]) {
 }
 
 /*
-USE ALARM SIGNAL FOR SYNCING FILES BETWEEN GROUP MEMBERS
+Use alarm signal for syncing files between group members
 */
 
 int main() {
@@ -72,12 +72,22 @@ int main() {
 
         if(ch == 1) {
             char in[30];
+            char* mulip;
             printf("Enter name of group to create: ");
             readLine(in);
-            insert(grpip, in, genMulticastIP());
+            genMulticastIP(mulip);
+            insert(grpip, in, mulip);
+
+            struct ip_mreq mreq;
+            mreq.imr_multiaddr.s_addr = inet_addr(mulip);
+            mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+            if(setsockopt(s, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+                printf("Error in creating group.\n");
+                exit(1);
+            }
 
         } else if(ch == 2) {
-            // SET SOCKET FOR BROADCAST
+            // Set socket for broadcast
             int on = 1;
             int s_temp;
             if((s_temp = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
@@ -88,16 +98,86 @@ int main() {
             setsockopt(s_temp, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on));
 
             struct sockaddr_in si_other;
-            char msg[10];
+            memset((char*) &si_other, 0, sizeof(si_other));
+            si_other.sin_family = AF_INET;
+            si_other.sin_port = htons(PORT);
+            si_other.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+            /*
+                Message Format:
+                B:keyword
+            */
+
+            char keyw[20], msg[22];
+            strcpy(msg, "B:");
+
+            printf("Enter keyword to search for groups: ");
+            scanf("%s", keyw);
+            strcat(msg, keyw);
+
             if(sendto(s_temp, msg, strlen(msg), 0, (struct sockaddr*) &si_other, sizeof(si_other)) == -1) {
                 printf("Error in sending broadcast message.\n");
                 exit(1);
             }
 
+            // Wait for reply for sometime
+            int retval;
+            fd_set rfds;
+            struct timeval tv;
+            tv.tv_sec = BROADCAST_TIMEOUT;
+            tv.tv_usec = 0;
+            FD_ZERO(&rfds);
+            FD_SET(s, &rfds);
+            retval = select(s_temp + 1, &rfds, NULL, NULL, &tv);
+
+            if(retval == 0) {
+                // Timeout happened
+                printf("Couldn't find any groups with the given keyword.\n");
+            } else {
+                // TODO: Handle replies
+            }
+
 
         } else if(ch == 3) {
+            // Group list hashmap is displayed, user can chose which group to join from this list
+            // To find more groups, search option can be used
+            char* reqip;
+
+            struct ip_mreq mreq;
+            mreq.imr_multiaddr.s_addr = inet_addr(reqip);
+            mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+            if(setsockopt(s, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+                printf("Error in joining group.\n");
+                exit(1);
+            }
+
+            printf("Successfully joined group.\n");
 
         } else if(ch == 4) {
+            /*
+                Message Format:
+                G:message
+            */
+
+            char gname[20], txtmsg[30], msg[32] = "G:";
+            printf("Enter group name to send message to: ");
+            readLine(gname);
+            printf("Enter the message: ");
+            readLine(txtmsg);
+            strcat(msg, txtmsg);
+            char* dest_ip = malloc(16);
+            dest_ip = get(grpip, gname);
+
+            struct sockaddr_in si_other;
+            memset((char*) &si_other, 0, sizeof(si_other));
+            si_other.sin_family = AF_INET;
+            si_other.sin_port = htons(PORT);
+            si_other.sin_addr.s_addr = inet_addr(dest_ip);
+
+            if(sendto(s, msg, strlen(msg), 0, (struct sockaddr*) &si_other, sizeof(si_other)) == -1) {
+                printf("Error in sending group message.\n");
+                exit(1);
+            }
 
         } else if(ch == 5) {
 
@@ -112,6 +192,7 @@ int main() {
 
     printf("Exiting...\n");
     close(s);
+    free_hm(grpip);
     return 0;
 
 }
